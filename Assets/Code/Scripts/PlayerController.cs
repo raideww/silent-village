e8b494c6ea2b1e24ad14765bbf71322ff4d58338
raideww <amir.khairullin@icloud.com>
2025-10-08 12:29:25 +0800

    Player Movement: Jump, Walking

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -1,20 +1,74 @@
+using System;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    void Start()
+    public InputActionAsset inputActions;
+
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+
+    private Vector2 moveValue;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    public float moveSpeed = 5.0f;
+    public float jumpSpeed = 10.0f;
+
+    private void OnEnable()
     {
-        
+        inputActions.FindActionMap("Player").Enable();
     }
-    
-    void Update()
+
+    private void OnDisable()
     {
-       
+        inputActions.FindActionMap("Player").Disable();
     }
-    
-    void FixedUpdate()
+
+    private void Awake()
     {
-        
+        moveAction = InputSystem.actions.FindAction("move");
+        jumpAction = InputSystem.actions.FindAction("jump");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveValue = moveAction.ReadValue<Vector2>();
+
+        if (jumpAction.WasPressedThisFrame())
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Walking();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            isGrounded = false;
+            rb.linearVelocityY = rb.linearVelocityY + jumpSpeed;
+        }
+    }
+
+    private void Walking()
+    {
+        rb.linearVelocityX = moveValue.x * moveSpeed;
     }
 }
