d49b43b613093a3404fafda8d31d24e46a6be4ac
raideww <amir.khairullin@icloud.com>
2025-10-13 20:40:05 +0800

    Add crouch

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 using System;
-using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,17 +9,24 @@ public class PlayerController : MonoBehaviour
     private InputAction moveAction;
     private InputAction jumpAction;
     private InputAction sprintAction;
+    private InputAction crouchAction;
 
 
     private Vector2 moveValue;
     private Rigidbody2D rb;
     private bool isGrounded;
     private bool isSprinting;
+    private bool isCrouching;
+    private Vector3 previousScale;
     private SpriteRenderer spriteRenderer;
+    private Renderer renderer;
+    
 
     public float moveSpeed = 5.0f;
-    public float jumpSpeed = 10.0f;
     public float sprintSpeed = 10.0f;
+    public float crouchSpeed = 2.5f;
+    public Vector3 crouchScale = new Vector3(1.0f, 0.7f, 1.0f);
+    public float jumpSpeed = 10.0f;
 
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
@@ -41,8 +47,11 @@ public class PlayerController : MonoBehaviour
         moveAction = InputSystem.actions.FindAction("move");
         jumpAction = InputSystem.actions.FindAction("jump");
         sprintAction = InputSystem.actions.FindAction("sprint");
+        crouchAction = InputSystem.actions.FindAction("crouch");
+        previousScale = transform.localScale;
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        renderer = GetComponent<Renderer>();
     }
 
     private void Update()
@@ -54,6 +63,37 @@ public class PlayerController : MonoBehaviour
             Jump();
         }
 
+        if (crouchAction.WasPressedThisFrame())
+        {
+            // Getting previous scale to take into account when calculating height compensation
+            // because when player's size changes it changes in center which leaves player off the ground
+            // thats why we should compensate this change
+            Vector2 beforeCrouchSize = renderer.bounds.size;
+
+            // Changing player's size
+            previousScale = transform.localScale;
+            transform.localScale = Vector3.Scale(transform.localScale, crouchScale);
+            isCrouching = true;
+
+            // Calculating height compensation by getting the difference of player's height before changing size and after
+            // and dividing it by 2 because we only need to account space under the player
+            Vector2 afterCrouchSize = renderer.bounds.size;
+            float heightCompensation = (beforeCrouchSize.y - afterCrouchSize.y) / 2;
+            Vector2 compensatedPosition = transform.localPosition - new Vector3(0, heightCompensation, 0);
+            transform.localPosition = compensatedPosition;
+        }
+        else if (crouchAction.WasReleasedThisFrame())
+        {
+            Vector2 beforeCrouchSize = renderer.bounds.size;
+
+            transform.localScale = previousScale;
+            isCrouching = false;
+
+            Vector2 afterCrouchSize = renderer.bounds.size;
+            float heightCompensation = (beforeCrouchSize.y - afterCrouchSize.y) / 2;
+            Vector2 compensatedPosition = transform.localPosition - new Vector3(0, heightCompensation, 0);
+            transform.localPosition = compensatedPosition;
+        }
         if (sprintAction.WasPerformedThisFrame())
         {
             isSprinting = true;
@@ -70,13 +110,14 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Jump()
-    {   isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         if (isGrounded)
         {
             rb.linearVelocityY = rb.linearVelocityY + jumpSpeed;
         }
     }
-
+    
     private void Walking()
     {
 
