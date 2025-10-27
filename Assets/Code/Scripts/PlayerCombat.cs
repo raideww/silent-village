25843a5b167fdb807b50e953e8bb441e09c68737
raideww <amir.khairullin@icloud.com>
2025-10-27 19:49:27 +0800

    Update playercombat script

--- Assets/Code/Scripts/PlayerCombat.cs
+++ Assets/Code/Scripts/PlayerCombat.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,19 +7,61 @@ public class PlayerCombat : MonoBehaviour
     private InputAction attackAction;
     private Animator animator;
 
+    // Attack
+    private bool isHolding = false;
+    private float holdingTime;
+    private float chargedHoldingTime = 1.0f;
+    private bool isCharged = false;
+    
+
     void Awake()
     {
         animator = GetComponent<Animator>();
         attackAction = InputSystem.actions.FindAction("attack");
     }
 
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
-        
+        if (isHolding)
+        {
+            HoldAttack();
+        }
+
+        if (attackAction.WasPressedThisFrame())
+        {
+            StartAttack();
+        }
+        if (attackAction.WasReleasedThisFrame())
+        {
+            EndAttack();
+        }
+    }
+
+    void StartAttack()
+    {
+        isHolding = true;
+    }
+
+    void EndAttack()
+    {
+        if (!isCharged)
+        {
+            animator.SetTrigger("attack");
+        }
+        else
+        {
+            animator.SetTrigger("chargedAttack");
+        }
+        isHolding = false;
+    }
+    
+    void HoldAttack()
+    {
+        holdingTime += Time.deltaTime;
+        if (holdingTime >= chargedHoldingTime)
+        {
+            isCharged = true;
+            animator.SetTrigger("chargedAttackHold");
+        }
     }
 }
