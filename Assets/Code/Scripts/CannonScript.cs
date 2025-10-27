a5a35c3749759a6f8df4e72a5bef1ade3ddff090
raideww <amir.khairullin@icloud.com>
2025-10-27 21:54:16 +0800

    add cannon ball & cannonscript

---
+++ Assets/Code/Scripts/CannonScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CannonScript : MonoBehaviour
+{
+    public Transform firePoint;
+    public float damage;
+    public GameObject cannonBallPrefab;
+    public float ballSpeed = 10.0f;
+
+    void Attack()
+    {
+        Vector3 spawnPoint = firePoint.transform.position + firePoint.rotation * Vector3.up;
+        GameObject ball = Instantiate(cannonBallPrefab, spawnPoint, firePoint.rotation);
+        Rigidbody2D ballRb = ball.GetComponent<Rigidbody2D>();
+        ballRb.linearVelocity = firePoint.rotation * (Vector3.up * ballSpeed);
+    }
+}
