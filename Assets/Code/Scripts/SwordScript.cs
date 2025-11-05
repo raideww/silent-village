6a924a3659650aba8aa4664df3cf1386a38f63ff
raideww <amir.khairullin@icloud.com>
2025-11-05 17:47:10 +0800

    aaa

--- Assets/Code/Scripts/SwordScript.cs
+++ Assets/Code/Scripts/SwordScript.cs
@@ -6,16 +6,7 @@ public class SwordScript : MonoBehaviour
     public float damage = 100.0f;
     public float damageCharged = 200.0f;
 
-<<<<<<< HEAD
     void OnCollisionEnter2D(Collision2D collision)
-=======
-    void Start()
-    {
-
-    }
-
-    void Update()
->>>>>>> main
     {
 
     }
