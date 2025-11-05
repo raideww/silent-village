ce429aa9d4efb7ad99de439b81b29feb17ab47db
raideww <amir.khairullin@icloud.com>
2025-11-05 21:47:12 +0800

    add different speeds

--- Assets/Code/Scripts/PlayerMovement.cs
+++ Assets/Code/Scripts/PlayerMovement.cs
@@ -12,8 +12,12 @@ enum MovementType
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Speed")]
-    public float walkingForce = 20.0f;
+    public float walkingForce = 5.0f;
     public float walkingSpeedMax = 10.0f;
+    public float sprintingForce = 20.0f;
+    public float sprintingSpeedMax = 20.0f;
+    public float crouchingForce = 2.0f;
+    public float crouchingSpeedMax = 5.0f;
 
     [Header("Drag")]
     public float dragAir = 0.05f;
@@ -37,12 +41,36 @@ public class PlayerMovement : MonoBehaviour
 
     private InputAction moveAction;
     private InputAction sprintAction;
+    private InputAction crouchAction;
+
     private Rigidbody2D rb;
 
     void Awake()
     {
         moveAction = InputSystem.actions.FindAction("move");
+        sprintAction = InputSystem.actions.FindAction("sprint");
+        crouchAction = InputSystem.actions.FindAction("crouch");
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (sprintAction.WasPressedThisFrame())
+        {
+            ChangeMovementType(MovementType.Sprinting);
+        }
+        else if (sprintAction.WasReleasedThisFrame() && currentMovementType == MovementType.Sprinting)
+        {
+            ChangeMovementType(MovementType.Walking);
+        }
+        if (crouchAction.WasPressedThisFrame())
+        {
+            ChangeMovementType(MovementType.Crouching);
+        }
+        else if (crouchAction.WasReleasedThisFrame() && currentMovementType == MovementType.Crouching)
+        {
+            ChangeMovementType(MovementType.Walking);
+        }
     }
 
     void FixedUpdate()
@@ -69,19 +97,41 @@ public class PlayerMovement : MonoBehaviour
         {
             if (Math.Abs(rb.linearVelocityX) < walkingSpeedMax)
             {
-                rb.AddForce(walkingForce * moveValue, ForceMode2D.Force);
+                rb.AddForce(walkingForce * moveValue);
+            }
+            else
+            {
+                rb.AddForce(walkingForce * moveValue * -1);
             }
         }
         else if (currentMovementType == MovementType.Sprinting)
         {
-
+            if (Math.Abs(rb.linearVelocityX) < sprintingSpeedMax)
+            {
+                rb.AddForce(sprintingForce * moveValue);
+            }
         }
         else if (currentMovementType == MovementType.Crouching)
         {
-
+            if (Math.Abs(rb.linearVelocityX) < crouchingSpeedMax)
+            {
+                rb.AddForce(crouchingForce * moveValue);
+            }
+            else
+            {
+                rb.AddForce(crouchingForce * moveValue * -1);
+            }
         }
 
         Debug.Log(rb.linearVelocity);
+    }
+
+    void ChangeMovementType(MovementType newMovementType)
+    {
+        if (newMovementType != currentMovementType)
+        {
+            currentMovementType = newMovementType;
+        }
     }
 
     bool GroundBelow()
